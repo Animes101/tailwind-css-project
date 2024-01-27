49ab8c9bfa0e32fe657673196a607a3fd1b3df65
animes barman <animesbarman101@gmail.com>
2024-01-27 17:16:40 +0600

    product area complated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['*'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Oranienbaum':['Oranienbaum', 'serif'],
+        'Oswald':['Oswald', 'sans-serif'],
+        'Roboto':['Roboto', 'sans-serif'],
+        'Overlock':['Overlock', 'sans-serif'],
+
+      },
+      colors: {
+        'text-color': '#000',
+      },
+      backgroundImage: {
+        'hero-img': "url('/images/hero.png')",
+        
+      }
+    },
   },
   plugins: [],
 }
